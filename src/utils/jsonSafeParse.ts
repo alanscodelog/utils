cee107a27745a82b515a6227a277f98ddf1123c0
Alan North <alanscodelog@gmail.com>
2026-03-14 01:02:46 -0300

    feat: jsonSafeParse now supports reviver function

--- src/utils/jsonSafeParse.ts
+++ src/utils/jsonSafeParse.ts
@@ -2,9 +2,10 @@ import { Err, Ok, type Result } from "./Result.js"
 
 export function jsonSafeParse<T>(
 	json: string,
+	reviver?: Parameters<typeof JSON.parse>[1],
 ): Result<T, Error> {
 	try {
-		return Ok(JSON.parse(json))
+		return Ok(JSON.parse(json, reviver))
 	} catch (err) {
 		return Err(err as any)
 	}
