7aba53a6de0f87dbbcf8f99452485a0d8035afd6
Alan North <alanscodelog@gmail.com>
2026-03-25 02:15:20 -0300

    feat: set now creates the object path if it doesn't exist

--- src/utils/set.ts
+++ src/utils/set.ts
@@ -3,14 +3,23 @@
  *
  * If keypath does not have any keys, the object will *not* be mutated, and the value to assign will just be returned.
  *
+ * If the path does not exist (the path is deeper than the object), it will be created. This WILL overwrite existing values unless they are objects or arrays that can't be written to with the given string, in those cases, careful, it WILL THROW.
+ *
  * ```ts
  * const obj = { a: { b: ["c"]} }
  * // mutates and returns the mutated obj
  * set(obj, ["a", "b", 0], "d")
  * // obj.a.b[0] is now "d"
  *
- * obj2 = set(obj, [], {}, {noError:true})
- * // obj is still the above, obj2 is {}
+ * const obj2 = {}
+ * set(obj2, ["a", "b", "c"], "d")
+ * // obj2.a.b.c is now "d"
+ *
+ * const obj3 = {a: "in the way"}
+ * set(obj3, ["a", "b", "c"], "d")
+ *
+ * const obj4 = {a: "in the way"}
+ * set(obj4, ["a", "b"], "d") // throws (we can't create a property b on a string)
  * ```
  */
 export function set(
@@ -23,7 +32,12 @@ export function set(
 	}
 	let lastObj = mutated
 	for (let i = 0; i < keypath.length - 1; i++) {
-		lastObj = lastObj[keypath[i]]
+		const key = keypath[i]
+		const value = lastObj[key]
+		if (value === undefined || i < keypath.length - 2) {
+			lastObj[keypath[i]] = {}
+		}
+		lastObj = lastObj[key]
 	}
 	lastObj[keypath[keypath.length - 1]] = val
 	return mutated
--- tests/utils/set.spec.ts
+++ tests/utils/set.spec.ts
@@ -25,3 +25,30 @@ it("works even if strings used for arrays", () => {
 	expect(obj).to.equal(value)
 	expect(obj.a.b[0]).to.equal("change")
 })
+
+it("works with deep paths on a shallow object", () => {
+	const obj = { }
+	const value = set(obj, ["a", "b", "c"], "changed")
+	expect(obj).to.deep.equal({
+		a: { b: { c: "changed" } }
+	})
+	expect(value).to.deep.equal(obj)
+	expect((obj as any).a.b.c).to.equal("changed")
+})
+
+it("overrides the needed path", () => {
+	const obj = { a: "will get overwritten" }
+	const value = set(obj, ["a", "b", "c"], "changed")
+	expect(obj).to.deep.equal({
+		a: { b: { c: "changed" } }
+	})
+	expect(value).to.deep.equal(obj)
+	expect((obj as any).a.b.c).to.equal("changed")
+})
+
+it("does not override arrays", () => {
+	const obj = { a: "will NOT get overwritten" }
+	expect(() => {
+		set(obj, ["a", "b"], "changed")
+	}).to.throw()
+})
